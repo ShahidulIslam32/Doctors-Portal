5f25a751b36b0691990f74fbd8ab661458a01d0f
ShahidulIslam32 <prherobatch05@gmail.com>
2022-05-19 10:14:45 +0600

    boking send data

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import swal from 'sweetalert';
 
 
 const BookingModal = ({date , treatment , setTreatment}) => {
     const {_id ,name , slots} = treatment;
+    const [user, loading, error] = useAuthState(auth);
+    const formatDate = format(date , 'PP')
+
     const handleFormSubmit = event => {
         event.preventDefault()
         const slot = event.target.slot.value
         console.log(slot , _id , name);
-        swal("Your Booking is Confirm", "Thank You For Being With Us", "success");
-        setTreatment(null)
+        // send data in database 
+
+        const booking = {
+            treatmentId : _id,
+            treatment: name,
+            date : formatDate,
+            slot,
+            pateint : user.email,
+            patientName : user.displayName,
+            phone : event.target.phone.value
+        }
+        // fetch data for send backend server
+
+        fetch('http://localhost:5000/booking', {
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            swal("Your Booking is Confirm", "Thank You For Being With Us", "success");
+            setTreatment(null)
+        })
 
     }
     return (
@@ -27,9 +55,11 @@ const BookingModal = ({date , treatment , setTreatment}) => {
                               slots.map(slot=><option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input name="name" type="text" placeholder="Full Name" className="input input-bordered w-full max-w-xs" required/>
-                        <input name="phone" type="text" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" required/>
-                        <input name="email" type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" required/>
+                        <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+
+                        <input name="phone" type="number" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" required/>
+
                         <input type="submit" value="Confirm Booking" className="input input-bordered bg-accent text-white w-full max-w-xs" />
                      </form>
                 </div>
